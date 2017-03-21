f44864a235cbf69c4d1b8b77b77c5d7f4bc6d9da
debarros <pj.debarros@gmail.com>
2017-03-21 14:28:10 -0400

    created functions.R

---
+++ functions.R
@@ -0,0 +1,17 @@
+#functions.R
+
+#Libraries ####
+library(openxlsx)
+library(magrittr)
+library(data.table)
+library(R6)
+
+
+#functions
+source("loadWorkbook.R")
+
+
+#Classes ####
+source("REPORTclass.R")
+source("RESULTclass.R")
+source("COMPARISONclass.R")
