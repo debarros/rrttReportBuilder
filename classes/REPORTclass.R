fa0cfdd67431b68fd877594ece930a7a366b3f94
debarros <pj.debarros@gmail.com>
2017-03-31 15:18:42 -0400

    cleaned up classes/REPORTclass.R

--- classes/REPORTclass.R
+++ classes/REPORTclass.R
@@ -32,68 +32,27 @@ REPORT = R6Class(
     Correlations = NULL, #correlations column from the ItemInfo
     ItemScores = NULL, #AverageScore column from the ItemInfo
     ItemResponseScores = NULL, #data.table with the score for every student on every item
-    DropScores = NULL #data.table with the score for each student after dropping each item
+    DropScores = NULL, #data.table with the score for each student after dropping each item
+    addr = paste0(getwd(),"/classes/REPORTclass/")
   ),
   
   public = list(
     initialize = function(TMS = "LinkIt"){private$TMS = TMS}, #default the Testing Management System to LinkIt
-    
     setDataLocation = function(x){private$DataLocation = x},
-    
     setComparisonLocation = function(x){private$ComparisonLocation = x},
-    
-    setSources = function(){ #get a list of files
-      source(paste0(getwd(),"/classes/REPORTclass/setSources.R"), local = T)
-    }, #setSources
-    
-    setTestName = function(){ #get the name of the test
-      source(paste0(getwd(),"/classes/REPORTclass/setTestName.R"), local = T)
-    }, #setTestName
-    
-    setItemInfo = function(){
-      source(paste0(getwd(),"/classes/REPORTclass/setItemInfo.R"), local = T)
-    }, #setItemInfo
-    
-    enhanceItemInfo = function(){ #load the complete item info from the comparison and topic alignment file
-      source(paste0(getwd(),"/classes/REPORTclass/enhanceItemInfo.R"), local = T)
-    }, #enhanceItemInfo
-    
-    setTopicAlignments = function(d2){
-      source(paste0(getwd(),"/classes/REPORTclass/setTopicAlignments.R"), local = T)
-    }, #setTopicAlignments
-    
-    addItemScores = function(){ #add the ItemResponseScores data.frame to each result and add item average scores to the ItemInfo
-      source(paste0(getwd(),"/classes/REPORTclass/addItemScores.R"), local = T)
-    }, #addItemScores
-    
-    setUploadTab = function(){
-      source(paste0(getwd(),"/classes/REPORTclass/setUploadTab.R"), local = T)
-    }, #setUploadTab
-    
-    setResults = function(){
-      source(paste0(getwd(),"/classes/REPORTclass/setResults.R"), local = T)
-    }, #setResults
-    
-    setItemSummary = function(x){
-      source(paste0(getwd(),"/classes/REPORTclass/setItemSummary.R"), local = T)
-    }, #setTopicSummary
-    
-    addCorrelations = function(){
-      source(paste0(getwd(),"/classes/REPORTclass/addCorrelations.R"), local = T)
-    }, #addCorrelations
-    
-    addResponseFrequencies = function(){  #ItemInfo
-      source(paste0(getwd(),"/classes/REPORTclass/addResponseFrequencies.R"), local = T)
-    }, #addResponseFrequencies
-    
-    getResponses = function(){
-      source(paste0(getwd(),"/classes/REPORTclass/getResponses.R"), local = T)
-    }, #getResponses
-    
-    badMessage = function(method){ #check whether a method can run right now
-      source(paste0(getwd(),"/classes/REPORTclass/badMessage.R"), local = T)
-    }, #badMessage
-    
+    setSources = function(){source(paste0(private$addr,"setSources.R"), local = T)},
+    setTestName = function(){source(paste0(private$addr,"setTestName.R"), local = T)},
+    setItemInfo = function(){source(paste0(private$addr,"setItemInfo.R"), local = T)},
+    enhanceItemInfo = function(){source(paste0(private$addr,"enhanceItemInfo.R"), local = T)},
+    setTopicAlignments = function(d2){source(paste0(private$addr,"setTopicAlignments.R"), local = T)},
+    addItemScores = function(){source(paste0(private$addr,"addItemScores.R"), local = T)},
+    setUploadTab = function(){source(paste0(private$addr,"setUploadTab.R"), local = T)},
+    setResults = function(){source(paste0(private$addr,"setResults.R"), local = T)},
+    setItemSummary = function(x){source(paste0(private$addr,"setItemSummary.R"), local = T)},
+    addCorrelations = function(){source(paste0(private$addr,"addCorrelations.R"), local = T)},
+    addResponseFrequencies = function(){source(paste0(private$addr,"addResponseFrequencies.R"), local = T)}, 
+    getResponses = function(){source(paste0(private$addr,"getResponses.R"), local = T)}, 
+    badMessage = function(method){source(paste0(private$addr,"badMessage.R"), local = T)}, 
     getSources = function(){return(private$Sources)},
     getTestName = function(){return(private$TestName)},
     getItemInfo = function(){return(private$ItemInfo)},
@@ -111,7 +70,7 @@ REPORT = R6Class(
     getCorrelations = function(){return(private$Correlations)},
     getResponseSet = function(){return(private$ResponseSet)},
     
-    #Methods still to be made
+    #Methods still to be made ####
     setSummary = function(x){private$Summary = x},
     setTopicSummary = function(x){private$ItemSummary = x},
     setNarrative = function(x){private$Narrative = x},
