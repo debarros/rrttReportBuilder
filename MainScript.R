91f10ae7e0bf0fc626b51263cdd307dd791c657a
debarros <pj.debarros@gmail.com>
2017-04-10 13:58:40 -0400

    added some lines and comments to MainScript.R

--- MainScript.R
+++ MainScript.R
@@ -7,7 +7,7 @@ source("functions.R")
 
 #Get the basic overview of stuff
 #dataLocation = choose.dir(default = "J:/tests/2016-2017/")  #select the folder
-currentReport = REPORT$new()
+currentReport = REPORT$new() #initiate a new report
 currentReport$setDataLocation("J:\\tests\\2016-2017\\Humanities\\H2\\week23 (2017-02-17) Cuba Africa Middle East\\exports")
 currentReport$getDataLocation() #just to check
 currentReport$setSources() #identify all the csv's in that folder
@@ -19,6 +19,7 @@ currentReport$getItemInfo() #just to check
 currentReport$setResults()  #Get the actual results
 currentReport$getResults()  #just to check
 currentReport$getResponses()  #just to check
+currentReport$getResponses2()  #just to check
 currentReport$setComparisonLocation("J:/tests/2016-2017/Humanities/H2/week23 (2017-02-17) Cuba Africa Middle East/comparison and topic alignment.xlsx")
 currentReport$getComparisonLocation()  #just to check
 currentReport$enhanceItemInfo()
@@ -31,10 +32,14 @@ currentReport$addCorrelations()
 currentReport$getCorrelations() #just to check
 currentReport$getItemInfo() #just to check
 currentReport$addResponseFrequencies()
-currentReport$getResponseSet()
+currentReport$getResponseSet() #just to check
+currentReport$getItemInfo() #just to check
 currentReport$setUploadTab()
-
-
+currentReport$getUploadTab() #just to check
+currentReport$setSummary()
+currentReport$getSummary() #just to check
+currentReport$setTopicSummary()
+currentReport$getTopicSummary() #just to check
 
 
 
