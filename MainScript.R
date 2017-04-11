398819de80f4147bb7ce1b3f56940f70a959ecdb
debarros <pj.debarros@gmail.com>
2017-04-11 13:30:42 -0400

    Updated MainScript.R to reflect current status of the project

--- MainScript.R
+++ MainScript.R
@@ -2,12 +2,9 @@
 
 source("functions.R")
 
-
-#load the exported linkit student item response files ####
-
-#Get the basic overview of stuff
 #dataLocation = choose.dir(default = "J:/tests/2016-2017/")  #select the folder
 currentReport = REPORT$new() #initiate a new report
+#currentReport$setDataLocation(dataLocation)
 currentReport$setDataLocation("J:\\tests\\2016-2017\\Humanities\\H2\\week23 (2017-02-17) Cuba Africa Middle East\\exports")
 currentReport$getDataLocation() #just to check
 currentReport$setSources() #identify all the csv's in that folder
@@ -19,11 +16,10 @@ currentReport$getItemInfo() #just to check
 currentReport$setResults()  #Get the actual results
 currentReport$getResults()  #just to check
 currentReport$getResponses()  #just to check
-currentReport$getResponses2()  #just to check
 currentReport$setComparisonLocation("J:/tests/2016-2017/Humanities/H2/week23 (2017-02-17) Cuba Africa Middle East/comparison and topic alignment.xlsx")
 currentReport$getComparisonLocation()  #just to check
 currentReport$enhanceItemInfo()
-#currentReport$setTopicAlignments() #run from inside enhanceItemInfo()
+#currentReport$setTopicAlignments() #gets run from inside enhanceItemInfo()
 currentReport$getItemInfo() #just to check
 currentReport$addItemScores()
 currentReport$getItemInfo() #just to check
@@ -41,5 +37,9 @@ currentReport$getSummary() #just to check
 currentReport$setTopicSummary()
 currentReport$getTopicSummary() #just to check
 
+currentReport$setItemSummary()
+currentReport$getItemSummary() #just to check
 
+
+currentReport
 
