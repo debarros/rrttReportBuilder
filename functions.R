48673513836d3f6d05bbf9afb091c116795462c8
debarros <pj.debarros@gmail.com>
2017-04-21 11:26:52 -0400

    Added year descriptions file and t.test2() function

--- functions.R
+++ functions.R
@@ -9,7 +9,7 @@ library(R6)
 
 #functions
 source("loadWorkbook.R")
-
+source("t.test2.R")
 
 #Classes ####
 source(paste0(getwd(), "/classes/REPORTclass.R"))
@@ -17,3 +17,5 @@ source(paste0(getwd(), "/classes/RESULTclass.R"))
 source(paste0(getwd(), "/classes/COMPARISONclass.R"))
 
 
+#data ####
+DescriptionLookup = read.csv("YearDescriptions.csv", stringsAsFactors = F)
