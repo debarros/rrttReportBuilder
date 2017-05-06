683cb2b3b625d9fbbf86a6a13ef499c8da51007f
debarros <pj.debarros@gmail.com>
2017-05-06 15:14:57 -0400

    generateReport.R - added 2 new parameters for the SMS and TMS, fixed a few lines that were written incorrectly, added exportUploads line, and added some comments

--- R/generateReport.R
+++ R/generateReport.R
@@ -3,17 +3,21 @@
 #' @param DataLocation a folder path
 #' @param ComparisonFileName The name of the file with the test set up and comparison info
 #' @param ReportFileName Desired name of the output excel file
+#' @param TMS Name of the testing management system.  Options currently limited to Linkit.
+#' @param SMS Name of the student management system.  Options currently limited to PowerSchool.
 #' @return an object of clas REPORT
 generateReport = function(DataLocation = choose.dir(default = "J:/tests/2016-2017/"),
                           ComparisonFileName = "comparison and topic alignment.xlsx",
-                          ReportFileName = "scores.xlsx"){
+                          ReportFileName = "scores.xlsx",
+                          TMS = "LinkIt",
+                          SMS = "PowerSchool"){
   currentReport = REPORT$new() #initiate a new report
   currentReport$setDataLocation(DataLocation)
   currentReport$setSources() #identify all the csv's in that folder
   currentReport$setTestName()
   currentReport$setItemInfo()
   currentReport$setResults()  #Get the actual results
-  currentReport$setComparisonFileName
+  currentReport$setComparisonFileName(ComparisonFileName)
   currentReport$setComparisonLocation(paste0(DataLocation, "\\", ComparisonFileName))
   currentReport$enhanceItemInfo()
   currentReport$addItemScores()
@@ -27,7 +31,8 @@ generateReport = function(DataLocation = choose.dir(default = "J:/tests/2016-201
   currentReport$setNarrative()
   currentReport$setTopicScores()
   currentReport$setHandouts()
-  currentReport$exportNarrative()
-  currentReport$exportReport(filename = ReportFileName)
+  currentReport$exportNarrative() #save the html file with the narrative
+  currentReport$exportReport(filename = ReportFileName) #save the xlsx score report file
+  currentReport$exportUploads() #save the csv for uploading grades
   return(currentReport)
 }
