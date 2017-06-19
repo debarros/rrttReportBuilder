ec670dd48afc77218b65bd8d2f9fc31b49c0d7e1
debarros <pj.debarros@gmail.com>
2017-06-19 19:03:24 -0400

    generateReport - added several parameters and lots of comments; added call to setUpLoadFiles

--- R/generateReport.R
+++ R/generateReport.R
@@ -5,26 +5,32 @@
 #' @param ReportFileName Desired name of the output excel file
 #' @param TMS Name of the testing management system.  Options currently limited to Linkit.
 #' @param SMS Name of the student management system.  Options currently limited to PowerSchool.
+#' @param useLocalValues logical: should the item values in the test setup file be used?
+#' @param useLocalNames logical: should the item names in the test setup file be used?
+#' @param UploadFilenames character of upload filenames.  The first is for percentage scores and the second is for total points.
 #' @return an object of class REPORT
 generateReport = function(DataLocation = choose.dir(default = "J:/tests/2016-2017/"),
                           ComparisonFileName = "comparison and topic alignment.xlsx",
                           ReportFileName = "scores.xlsx",
-                          TMS = "LinkIt",
-                          SMS = "PowerSchool"){
+                          TMS = "LinkIt", SMS = "PowerSchool",
+                          useLocalValues = F, useLocalNames = F, 
+                          UploadFilenames = c("upload_percentages.csv", "upload_totalpoints.csv")){
   currentReport = REPORT$new() #initiate a new report
-  currentReport$setDataLocation(DataLocation)
-  currentReport$setSources() #identify all the csv's in that folder
+  currentReport$setUpLoadFiles(UploadFilenames) # set the names of the upload files
+  currentReport$setDataLocation(DataLocation) # set the folder for the test
+  currentReport$setSources() #identify all the csv's in the exports subfolder
   currentReport$setTestName()
   currentReport$setItemInfo()
   currentReport$setResults()  #Get the actual results
   currentReport$setComparisonFileName(ComparisonFileName)
   currentReport$setComparisonLocation(paste0(DataLocation, "\\", ComparisonFileName))
-  currentReport$enhanceItemInfo() # apply local item names, set topic alignments, set the type and options 
+  # apply local item names and values, set topic alignments, set the type and number of options 
+  currentReport$enhanceItemInfo(report = currentReport, useLocalNames = useLocalNames, useLocalValues = useLocalValues) 
   currentReport$addItemScores()
   currentReport$loadSpecialScoring()  # Load special scoring rules
   currentReport$applySpecialScoring()  # Apply special scoring rules
-  currentReport$addCorrelations()
-  currentReport$addResponseFrequencies()
+  currentReport$addCorrelations() # add item correlations to ItemInfo and to Correlations
+  currentReport$addResponseFrequencies() # add ReponseSet and add response frequencies to ItemInfo
   currentReport$setUploadTab()
   currentReport$setSummary()
   currentReport$setTopicSummary()
@@ -35,6 +41,6 @@ generateReport = function(DataLocation = choose.dir(default = "J:/tests/2016-201
   currentReport$setHandouts()
   currentReport$exportNarrative() #save the html file with the narrative
   currentReport$exportReport(filename = ReportFileName) #save the xlsx score report file
-  currentReport$exportUploads() #save the csv for uploading grades
+  currentReport$exportUploads() #save the CSV's for uploading grades
   return(currentReport)
-}
+} # /function
