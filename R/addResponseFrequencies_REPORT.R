b978e0ea5139ddc5fa8fd7462f7d4154b9529797
debarros <pj.debarros@gmail.com>
2017-06-19 19:00:38 -0400

    addResponseFrequencies - added code to make a local variable for ItemInfo; changed code for responseSet to account for the case that there are not MC questions

--- R/addResponseFrequencies_REPORT.R
+++ R/addResponseFrequencies_REPORT.R
@@ -9,14 +9,15 @@ addResponseFrequencies.REPORT = function(report) {
     return(badmessage)
   } else {
     ItemResponses = report$getResponses()
+    ItemInfo = report$getItemInfo()
     
     #Set default values for the number of letter options and number of point options
     topletter = 0
     toppoint = 0
     
     #Find the max number of letter and point options in the test
-    if("MC" %in% report$.__enclos_env__$private$ItemInfo$Type){topletter = max(report$.__enclos_env__$private$ItemInfo$options, na.rm = T)}
-    if("ER" %in% report$.__enclos_env__$private$ItemInfo$Type){toppoint = max(report$.__enclos_env__$private$ItemInfo$Value[report$.__enclos_env__$private$ItemInfo$Type == "ER"], na.rm = T)} 
+    if("MC" %in% ItemInfo$Type){topletter = max(ItemInfo$options, na.rm = T)}
+    if("ER" %in% ItemInfo$Type){toppoint = max(ItemInfo$Value[ItemInfo$Type == "ER"], na.rm = T)} 
     
     # Find the max number of response columns needed and the number 
     # of columns that will represent both numbers and letters
@@ -24,12 +25,17 @@ addResponseFrequencies.REPORT = function(report) {
     overlapset = min(topletter, toppoint+1, na.rm = T)
     
     #Build the set of response column names
-    responseSet = paste0(
-      c(LETTERS[1:topletter], rep("", times = totalset - topletter)),
-      c(rep("/", times = overlapset), rep("", times = totalset - overlapset)),
-      c(as.character(0:toppoint), rep("", times = totalset - (toppoint+1) )))
+    if("MC" %in% ItemInfo$Type){
+      responseSet = paste0(
+        c(LETTERS[1:topletter], rep("", times = totalset - topletter)),
+        c(rep("/", times = overlapset), rep("", times = totalset - overlapset)),
+        c(as.character(0:toppoint), rep("", times = totalset - (toppoint+1) )))  
+    } else {
+      responseSet = paste0(
+        c(as.character(0:toppoint), rep("", times = totalset - (toppoint+1) )))  
+    }
     
-    basecolumn = ncol(report$.__enclos_env__$private$ItemInfo) #How many columns does ItemInfo have already?
+    basecolumn = ncol(ItemInfo) #How many columns does ItemInfo have already?
     report$.__enclos_env__$private$ItemInfo[,responseSet] = "" #Initialize those columns
     
     #This nested loop should be rewritten using lapply or something
@@ -41,9 +47,9 @@ addResponseFrequencies.REPORT = function(report) {
         } else { #if it's an MC item, count how many times that letter was used
           report$.__enclos_env__$private$ItemInfo[i,j+basecolumn] = sum(
             ItemResponses[,report$.__enclos_env__$private$ItemInfo$ItemName[i], with = F] == LETTERS[j], na.rm = T)
-        }
-      }
-    }
+        } # /if-else
+      } # /for each possible response
+    } # /for each item
     report$.__enclos_env__$private$ResponseSet = responseSet
   }
 }
