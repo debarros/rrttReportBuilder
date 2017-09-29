cbc9312467243e8eb4c67ea54a13aefff0372319
debarros <pj.debarros@gmail.com>
2017-09-29 18:46:32 -0400

    in the addItemScores method of the REPORT class, cleaned up the code; added the TMS parameter to the call to the setItemResponseScore method of the RESULT class; added comments

--- R/addItemScores_REPORT.R
+++ R/addItemScores_REPORT.R
@@ -3,12 +3,15 @@
 addItemScores.REPORT = function(report) {
   # put badmessage call here
   
+  ItemInfo = report$getItemInfo()
+  TMS = report$getTMS()
+  
   #establish a list that will hold the Item Response Scores data.frames
   ItReScores = vector(mode = "list", length = length(report$getResults()))
   
   #calculate the item response scores for each section and load them in the list
   for(i in 1:length(report$getResults())){
-    report$getResults()[[i]]$setItemResponseScores(report$getItemInfo())
+    report$getResults()[[i]]$setItemResponseScores(ItemInfo = report$getItemInfo(), TMS = TMS)
     ItReScores[[i]] = report$getResults()[[i]]$getItemResponseScores()
   }
   
@@ -16,11 +19,11 @@ addItemScores.REPORT = function(report) {
   ItReScores = data.table::rbindlist(ItReScores) 
   
   #Calculate the average score for each question
-  for(i in 1:nrow(report$getItemInfo())){
-    report$.__enclos_env__$private$ItemInfo$AverageScore[i] = mean(
-      ItReScores[[report$.__enclos_env__$private$ItemInfo$ItemName[i]]])/report$.__enclos_env__$private$ItemInfo$Value[i]
+  for(i in 1:nrow(ItemInfo)){
+    ItemInfo$AverageScore[i] = mean(ItReScores[[ItemInfo$ItemName[i]]])/ItemInfo$Value[i]
   }
-  report$.__enclos_env__$private$ItemScores = report$.__enclos_env__$private$ItemInfo$AverageScore
+  report$.__enclos_env__$private$ItemInfo = ItemInfo
+  report$.__enclos_env__$private$ItemScores = ItemInfo$AverageScore
   report$.__enclos_env__$private$ItemResponseScores = ItReScores
   
-}
+} # /addItemScores.REPORT function
