f75298ea6cf742b001fc643bb50c80dddef55cf1
debarros <pj.debarros@gmail.com>
2017-09-29 16:37:36 -0400

    generateReport.R - removed the useLocalValues and useLocalNames parameters; added the TMS variable to the call to REPORT$new; Removed the call to enhanceItemInfo; moved the calls to setItemInfo and setResults to after the comparison filename and path are set.

--- R/generateReport.R
+++ R/generateReport.R
@@ -3,7 +3,7 @@
 #' @param DataLocation a folder path
 #' @param ComparisonFileName The name of the file with the test set up and comparison info
 #' @param ReportFileName Desired name of the output excel file
-#' @param TMS Name of the testing management system.  Options currently limited to Linkit.
+#' @param TMS Name of the testing management system.  Options currently limited to Linkit and ScantronAS.
 #' @param SMS Name of the student management system.  Options currently limited to PowerSchool.
 #' @param useLocalValues logical: should the item values in the test setup file be used?
 #' @param useLocalNames logical: should the item names in the test setup file be used?
@@ -13,19 +13,16 @@ generateReport = function(DataLocation = choose.dir(default = "J:/tests/2016-201
                           ComparisonFileName = "comparison and topic alignment.xlsx",
                           ReportFileName = "scores.xlsx",
                           TMS = "LinkIt", SMS = "PowerSchool",
-                          useLocalValues = F, useLocalNames = F, 
                           UploadFilenames = c("upload_percentages.csv", "upload_totalpoints.csv")){
-  currentReport = REPORT$new() #initiate a new report
+  currentReport = REPORT$new(TMS = TMS) #initiate a new report
   currentReport$setUpLoadFiles(UploadFilenames) # set the names of the upload files
   currentReport$setDataLocation(DataLocation) # set the folder for the test
   currentReport$setSources() #identify all the csv's in the exports subfolder
   currentReport$setTestName()
-  currentReport$setItemInfo()
-  currentReport$setResults()  #Get the actual results
   currentReport$setComparisonFileName(ComparisonFileName)
   currentReport$setComparisonLocation(paste0(DataLocation, "\\", ComparisonFileName))
-  # apply local item names and values, set topic alignments, set the type and number of options 
-  currentReport$enhanceItemInfo(report = currentReport, useLocalNames = useLocalNames, useLocalValues = useLocalValues) 
+  currentReport$setItemInfo()
+  currentReport$setResults()  #Get the actual results
   currentReport$addItemScores()
   currentReport$loadSpecialScoring()  # Load special scoring rules
   currentReport$applySpecialScoring()  # Apply special scoring rules
