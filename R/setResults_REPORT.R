48e87f8ed7988ab404d18ced8832917067a64f4e
debarros <pj.debarros@gmail.com>
2017-09-29 18:43:01 -0400

    in the setResults method for the REPORT class, cleaned up code; added functionality to act differently depending on  the TMS

--- R/setResults_REPORT.R
+++ R/setResults_REPORT.R
@@ -1,24 +1,47 @@
 # setResults_REPORT
 
 setResults.REPORT = function(report) {
-      if(is.null(report$.__enclos_env__$private$Sources)){
-        return("Need sources first.")
-      } else {
-        #set up list to hold the response sets for the various sections
-        results = vector(mode = "list", length = length(report$.__enclos_env__$private$Sources)) 
-        #add names to the list so they can be set later
-        names(results) = paste0("a", 1:length(report$.__enclos_env__$private$Sources)) 
-        for (i in 1:length(report$.__enclos_env__$private$Sources)){  #for each source/section
-          SectionName = read.csv(file = report$.__enclos_env__$private$Sources[i], 
-                                 skip = 1, 
-                                 header = F, 
-                                 nrows = 1, 
-                                 stringsAsFactors = F)[1,2] #get the section name
-          thisResult = RESULT$new(SectionName)
-          thisResult$setItemResponses(report$.__enclos_env__$private$Sources[i], report$.__enclos_env__$private$ItemInfo$ItemName, report$.__enclos_env__$private$ItemInfo$Value)
-          results[[i]] = thisResult #put the response info in the list
-          names(results)[i] = SectionName #set the element name in the list to be the name of the section
-        }
-        report$.__enclos_env__$private$Results = results
-      }
-}
+  
+  Sources = report$getSources()
+  nsources = length(Sources)
+  ItemInfo = report$getItemInfo()
+  TMS = report$getTMS()
+  
+  if(TMS == "ScantronAS"){
+    sourcenames = report$.__enclos_env__$private$SourceFileNames
+    #set up list to hold the response sets for the various sections
+    results = vector(mode = "list", length = nsources) 
+    #add names to the list so they can be set later
+    names(results) = paste0("a", 1:nsources) 
+    for (i in 1:nsources){  #for each source/section
+      thisSource = sourcenames[i]
+      SectionName = strsplit(x = thisSource, split = "_")[[1]]
+      SectionName = paste0(SectionName[1:(length(SectionName)-1)], collapse = " ")
+      thisResult = RESULT$new(SectionName)
+      thisResult$setItemResponses(Sources[i], ItemInfo$ItemName, ItemInfo$Value, TMS)
+      results[[i]] = thisResult #put the response info in the list
+      names(results)[i] = SectionName #set the element name in the list to be the name of the section
+    }
+    report$.__enclos_env__$private$Results = results
+    
+  } else if(TMS == "LinkIt"){
+    #set up list to hold the response sets for the various sections
+    results = vector(mode = "list", length = nsources) 
+    #add names to the list so they can be set later
+    names(results) = paste0("a", 1:nsources) 
+    for (i in 1:nsources){  #for each source/section
+      SectionName = read.csv(file = Sources[i], 
+                             skip = 1, header = F, nrows = 1, 
+                             stringsAsFactors = F)[1,2] #get the section name
+      thisResult = RESULT$new(SectionName)
+      thisResult$setItemResponses(Sources[i], ItemInfo$ItemName, ItemInfo$Value, TMS)
+      results[[i]] = thisResult #put the response info in the list
+      names(results)[i] = SectionName #set the element name in the list to be the name of the section
+    }
+    report$.__enclos_env__$private$Results = results
+    
+  } else {
+    stop(paste0("Unknown or unsupported TMS: ", TMS))
+  } # /if-else
+  
+} # /setResults.REPORT function
