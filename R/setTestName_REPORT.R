8d4530bcc3955bc25d90298f423a983edf0a5628
debarros <pj.debarros@gmail.com>
2017-09-29 16:38:45 -0400

    modified the setTestName method of the REPORT method to be able to get the test name from either LinkIt or ScantronAS exports

--- R/setTestName_REPORT.R
+++ R/setTestName_REPORT.R
@@ -1,9 +1,20 @@
 # setTestName_REPORT
 
 setTestName.REPORT = function(report) {
-      if(is.null(report$.__enclos_env__$private$Sources)){
-        return("Need sources first.")
-      } else {
-        report$.__enclos_env__$private$TestName = read.csv(report$.__enclos_env__$private$Sources[1], header = F, nrows = 1, stringsAsFactors = F)[1,2] 
+  if(report$getTMS() == "LinkIt"){
+    report$.__enclos_env__$private$TestName = read.csv(report$.__enclos_env__$private$Sources[1], header = F, nrows = 1, stringsAsFactors = F)[1,2] 
+  } else if(report$getTMS() == "ScantronAS"){
+    i = 0
+    testname = NA_character_
+    while(is.na(testname)){
+      i = i+1
+      if(i > length(report$.__enclos_env__$private$Sources)){
+        stop("There is no data")
       }
-}
+      testname = read.csv(report$.__enclos_env__$private$Sources[i], stringsAsFactors = F)[1,3]
+    }
+    report$.__enclos_env__$private$TestName = testname
+  } else {
+    stop(paste0("Unknown TMS: ",report$getTMS()))  
+  } # /if-else
+} # /function
