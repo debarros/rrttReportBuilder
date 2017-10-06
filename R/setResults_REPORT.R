04ce420a480d0710c879bff06dc4c990eec85a9b
debarros <pj.debarros@gmail.com>
2017-10-06 09:27:06 -0400

    in the setResults method of the REPORT class, added code to pull the section names from the export file names when the TMS is ScantronAS.  Also added code to set the missingSections value

--- R/setResults_REPORT.R
+++ R/setResults_REPORT.R
@@ -9,20 +9,29 @@ setResults.REPORT = function(report) {
   
   if(TMS == "ScantronAS"){
     sourcenames = report$.__enclos_env__$private$SourceFileNames
-    #set up list to hold the response sets for the various sections
-    results = vector(mode = "list", length = nsources) 
-    #add names to the list so they can be set later
-    names(results) = paste0("a", 1:nsources) 
+    SectionNames = strsplit(x = sourcenames, split = "_")
+    for(i in 1:length(sourcenames)){
+      x = SectionNames[[i]]
+      SectionNames[[i]] = paste0(x[1:(length(x)-1)], collapse = " ")
+    } # /for
+    results = vector(mode = "list", length = nsources) # set up list to hold the response sets for the various sections
+    hasData = vector(mode = "logical", length = nsources) # set up a logical vector to hold whether or not the result has any data
+    names(results) = paste0("a", 1:nsources)  #add names to the list so they can be set later
     for (i in 1:nsources){  #for each source/section
       thisSource = sourcenames[i]
-      SectionName = strsplit(x = thisSource, split = "_")[[1]]
-      SectionName = paste0(SectionName[1:(length(SectionName)-1)], collapse = " ")
+      SectionName = SectionNames[[i]]
       thisResult = RESULT$new(SectionName)
-      thisResult$setItemResponses(Sources[i], ItemInfo$ItemName, ItemInfo$Value, TMS)
-      results[[i]] = thisResult #put the response info in the list
+      hasData[i] = thisResult$setItemResponses(sourceLocation = Sources[i], itemNames = ItemInfo$ItemName, itemValues = ItemInfo$Value, TMS = TMS)
+      if(hasData[i]){
+        results[[i]] = thisResult #put the response info in the list
+      }
       names(results)[i] = SectionName #set the element name in the list to be the name of the section
-    }
-    report$.__enclos_env__$private$Results = results
+    } # for each source/section
+    missingSections = SectionNames[!hasData]
+    missingSections = unlist(missingSections)
+    
+    report$.__enclos_env__$private$MissingSections = missingSections
+    report$.__enclos_env__$private$Results = results[hasData]
     
   } else if(TMS == "LinkIt"){
     #set up list to hold the response sets for the various sections
