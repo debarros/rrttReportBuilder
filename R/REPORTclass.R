5becfdbf0ac93cc6b479b5e5cc0235c30f578fea
debarros <pj.debarros@gmail.com>
2017-10-18 12:51:21 -0400

    Organized and alphabetized stuff in the REPORT class

--- R/REPORTclass.R
+++ R/REPORTclass.R
@@ -2,12 +2,6 @@
 
 #An instance of this class holds everything necessary for a score report on a single test
 
-#How to deal with questions where the item name is the setup file differs from the item name in the export?
-#This becomes an issue when running the enhanceItemInfo method
-#At that point, the ItemInfo member of the report object and the ItemResponses member of each RESULT object 
-# already have the item names from the export file
-#There should probably be a an object that shows the correspondence between the two sets of item names
-
 # need a reason to use setPassingScore - where is this encoded?  How is the call triggered?
 
 REPORT = R6Class(
@@ -15,6 +9,19 @@ REPORT = R6Class(
   classname = "REPORT",
   
   private = list(
+    
+    # General parameters
+    DistractorCutoffProportion = 0.25, #cutoff percentage for determining whether an item is a distrator
+    OverThinkCutoff = 0.1, #cutoff correlation for determining whether an item is an overthinker
+    EasyCutoff = 0.9, #cutoff score for determining whether an item is easy
+    RelatedCutoffProportion = 0.2, #target proportion of items to count as highly related
+    PassingScore = 0.7, #passing score for the test
+    #     lower bound, upper bound, and target proportion for criterion for determining whether an item is difficult
+    DifficultCutoffParams = list("Lower" = 0.4, "Upper" = 0.5, "Proportion" = 0.2), 
+    #     rules for determining whether an item is Wheat From Chaff
+    ChaffRules = data.frame(score = c(.3, .4), correlation = c(.3, .5)), 
+    
+    # General properties
     TMS = NULL, #The Testing Management System
     DataLocation = NULL, #address of the folder for the test
     ComparisonLocation = NULL, #address and filename of the comparison and topic alignment
@@ -22,7 +29,19 @@ REPORT = R6Class(
     Sources = NULL, #character vector with the locations of the csv's
     MissingSections = NULL, # character vector with the names of sections for which there is no data
     TestName = NULL, #atomic character with the name of the test
+    ComparisonFileName = "comparison and topic alignment.xlsx", #test setup info filename (no file path)
+    UpLdFileNames = NULL, # character with the names of the percentage and total points upload files
+    HasTopics = NULL, # logical that indicates whether the test has topic alignments
+    
+    # Item stuff
     ItemInfo = NULL, #data.frame with info about the items, will be used to build the breakdown tab
+    ResponseSet = NULL, #character vector hold the names of the different response frequencies columns
+    Correlations = NULL, #correlations column from the ItemInfo
+    ItemScores = NULL, #AverageScore column from the ItemInfo
+    ItemResponseScores = NULL, #data.table with the score for every student on every item
+    DropScores = NULL, #data.table with the score for each student after dropping each item
+    
+    # Report components
     UploadTab = NULL, #data.frame that holds the stuff that goes in the upload tab and upload_percentages export
     UploadTotalPoints = NULL, # data that holds the stuff that goes in the upload_totalpoints export
     Results = NULL, #list of objects of class RESULT
@@ -34,99 +53,94 @@ REPORT = R6Class(
     Narrative = NULL, #Character vector. Text in cell A11 of Item Summary tab, but with markdown formatting
     Comparison = NULL, #a list of objects of class COMPARISON
     Handouts = NULL, #data.frame containing the information necessary to build the Handouts tab simply
-    DistractorCutoffProportion = 0.25, #cutoff percentage for determining whether an item is a distrator
-    OverThinkCutoff = 0.1, #cutoff correlation for determining whether an item is an overthinker
-    EasyCutoff = 0.9, #cutoff score for determining whether an item is easy
-    #lower bound, upper bound, and target proportion for criterion for determining whether an item is difficult
-    DifficultCutoffParams = list("Lower" = 0.4, "Upper" = 0.5, "Proportion" = 0.2), 
-    #rules for determining whether an item is Wheat From Chaff
-    ChaffRules = data.frame(score = c(.3, .4), correlation = c(.3, .5)), 
-    RelatedCutoffProportion = 0.2, #target proportion of items to count as highly related
-    ResponseSet = NULL, #character vector hold the names of the different response frequencies columns
-    Correlations = NULL, #correlations column from the ItemInfo
-    ItemScores = NULL, #AverageScore column from the ItemInfo
-    ItemResponseScores = NULL, #data.table with the score for every student on every item
-    DropScores = NULL, #data.table with the score for each student after dropping each item
-    PassingScore = 0.7, #passing score for the test
-    ComparisonFileName = "comparison and topic alignment.xlsx", #test setup info filename (no file path)
+    
+    # Scoring info
     SpecialScoring = NULL, # list of special scoring rules
     HasSpecialScoring = NULL, # logical that indicates whether the test has special scoring
     HasStudentScoring = NULL, # logical that indicates whether the special scoring is different for different students
-    SpecialScoringTable = NULL, # data.frame showing what special scoring rule to use for each student
-    UpLdFileNames = NULL, # character with the names of the percentage and total points upload files
-    HasTopics = NULL # logical that indicates whether the test has topic alignments
+    SpecialScoringTable = NULL # data.frame showing what special scoring rule to use for each student
+    
   ), # /private
   
   public = list(
+    
+    # Initialize method - new()
     initialize = function(TMS = "LinkIt"){private$TMS = TMS}, #default the Testing Management System to LinkIt
-    setDataLocation = function(x){private$DataLocation = x},
+    
+    # Methods to set members
+    setComparison = function(report = self){ setComparison.REPORT(report) }, 
     setComparisonFileName = function(x){private$ComparisonFileName = x},
     setComparisonLocation = function(x){private$ComparisonLocation = x},
-    setSources = function(report = self){ setSources.REPORT(report) }, 
-    setTestName = function(report = self){ setTestName.REPORT(report) }, 
+    setDataLocation = function(x){private$DataLocation = x},
     setItemInfo = function(report = self){ setItemInfo.REPORT(report) }, 
-    enhanceItemInfo = function(report = self, useLocalNames = F, useLocalValues = F){
-      enhanceItemInfo.REPORT(report, useLocalNames, useLocalValues) 
-    }, 
-    setTopicAlignments = function(d2, report = self){ setTopicAlignments.REPORT(d2, report) }, 
-    addItemScores = function(report = self){ addItemScores.REPORT(report) },
-    setUploadTab = function(report = self){ setUploadTab.REPORT(report) },
-    setResults = function(report = self){ setResults.REPORT(report) }, 
-    setPassingScore = function(x){private$PassingScore = x},
-    addCorrelations = function(report = self){ addCorrelations.REPORT(report) }, 
-    addResponseFrequencies = function(report = self){ addResponseFrequencies.REPORT(report) },
-    getResponses = function(report = self){ return(getResponses.REPORT(report)) }, 
-    badMessage = function(method, report = self){ return(badMessage.REPORT(method, report)) },
-    getSources = function(){return(private$Sources)},
-    getTestName = function(){return(private$TestName)},
-    getItemInfo = function(){return(private$ItemInfo)},
-    getUploadTab = function(){return(private$UploadTab)},
-    getUploadTotalPoints = function(){return(private$UploadTotalPoints)},
-    getDataLocation = function(){return(private$DataLocation)},
-    getComparisonLocation = function(){return(private$ComparisonLocation)},
-    getResults = function(){return(private$Results)},
-    getTopicSummary = function(){return(private$TopicSummary)},
-    getSummary = function(){return(private$Summary)},
-    getItemSummary = function(){return(private$ItemSummary)},
-    getItemScores = function(){return(private$ItemScores)},
-    getNarrative = function(){return(private$Narrative)},
-    getComparison = function(){return(private$Comparison)},
-    getHandouts = function(){return(private$Handouts)},
-    getCorrelations = function(){return(private$Correlations)},
-    getResponseSet = function(){return(private$ResponseSet)},
-    getPassingScore = function(){return(private$PassingScore)},
-    getTopicAlignments = function(report = self){ return(getTopicAlignments.REPORT(report)) }, 
-    setSummary = function(report = self){ setSummary.REPORT(report) }, 
     setItemSummary = function(report = self){ setItemSummary.REPORT(report) }, 
-    setTopicSummary = function(report = self){ setTopicSummary.REPORT(report) }, 
-    setComparison = function(report = self){ setComparison.REPORT(report) }, 
-    setNarrative = function(report = self){ setNarrative.REPORT(report) },
-    exportNarrative = function(report = self){ exportNarrative.REPORT(report) }, 
-    exportReport = function(report = self, filename = "scores.xlsx"){ 
-      exportReport.REPORT(report, filename) 
-    }, 
-    setTopicScores = function(report = self){ setTopicScores.REPORT(report) }, 
-    getTopicScores = function(){return(private$TopicScores)},
     setHandouts = function(report = self){ setHandouts.REPORT(report) }, 
-    exportUploads = function(report = self){ exportUploads.REPORT(report) }, 
-    applySpecialScoring = function(report = self){ applySpecialScoring.REPORT(report) },
-    loadSpecialScoring = function(report = self){ loadSpecialScoring.REPORT(report) }, 
+    setNarrative = function(report = self){ setNarrative.REPORT(report) },
+    setPassingScore = function(x){private$PassingScore = x},
+    setResults = function(report = self){ setResults.REPORT(report) }, 
+    setSources = function(report = self){ setSources.REPORT(report) }, 
+    setSummary = function(report = self){ setSummary.REPORT(report) }, 
+    setTestName = function(report = self){ setTestName.REPORT(report) }, 
+    setTopicAlignments = function(d2, report = self){ setTopicAlignments.REPORT(d2, report) }, 
+    setUploadTab = function(report = self){ setUploadTab.REPORT(report) },
+    setTopicSummary = function(report = self){ setTopicSummary.REPORT(report) }, 
+    setTopicScores = function(report = self){ setTopicScores.REPORT(report) }, 
+    setUpLoadFiles = function(filenames = c("upload_percentages.csv", "upload_totalpoints.csv")){
+      private$UpLdFileNames = filenames},
+    
+    # Methods to return members
+    getComparisonLocation = function(){return(private$ComparisonLocation)},
+    getComparison = function(){return(private$Comparison)},
+    getCorrelations = function(){return(private$Correlations)},
+    getDataLocation = function(){return(private$DataLocation)},
+    getHandouts = function(){return(private$Handouts)},
+    getItemInfo = function(){return(private$ItemInfo)},
+    getItemScores = function(){return(private$ItemScores)},
+    getItemSummary = function(){return(private$ItemSummary)},
+    getMissingSections = function(){return(private$MissingSections)},
+    getNarrative = function(){return(private$Narrative)},
+    getPassingScore = function(){return(private$PassingScore)},
+    getResponses = function(report = self){ return(getResponses.REPORT(report)) }, 
+    getResponseSet = function(){return(private$ResponseSet)},
+    getResults = function(){return(private$Results)},
+    getSources = function(){return(private$Sources)},
     getSpecialScoring = function(){private$SpecialScoring},
     getSpecialScoringTable = function(){return(private$SpecialScoringTable)},
-    updateIRandIRS = function(report = self){updateIRandIRS.REPORT(self)},
-    updateItemInfo = function(x){private$ItemInfo = x},
-    checkTopics = function(){return(private$HasTopics)},
+    getSummary = function(){return(private$Summary)},
+    getTestName = function(){return(private$TestName)},
+    getTopicSummary = function(){return(private$TopicSummary)},
+    getUploadTab = function(){return(private$UploadTab)},
+    getUploadTotalPoints = function(){return(private$UploadTotalPoints)},
+    getTMS = function(){return(private$TMS)},
+    getTopicAlignments = function(report = self){ return(getTopicAlignments.REPORT(report)) }, 
+    getTopicScores = function(){return(private$TopicScores)},
+    getUpLoadFiles = function(){return(private$UpLdFileNames)},
+    
+    # Methods to quick set members
+    
+    # Export Methods
+    exportNarrative = function(report = self){ exportNarrative.REPORT(report) }, 
+    exportReport = function(report = self, filename = "scores.xlsx"){ 
+      exportReport.REPORT(report, filename) }, 
+    exportUpdate = function(uploadFilePath, report = self){ # called from exportReport
+      exportUpdate.REPORT(uploadFilePath, report) },
+    exportUploads = function(report = self){ exportUploads.REPORT(report) }, 
+    
+    # Other methods
+    addCorrelations = function(report = self){ addCorrelations.REPORT(report) }, 
+    addItemScores = function(report = self){ addItemScores.REPORT(report) },
+    addResponseFrequencies = function(report = self){ addResponseFrequencies.REPORT(report) },
+    applySpecialScoring = function(report = self){ applySpecialScoring.REPORT(report) },
+    badMessage = function(method, report = self){ return(badMessage.REPORT(method, report)) },
     checkSpecScor = function(){return(private$HasSpecialScoring)},
     checkStudScor = function(){return(private$HasStudentScoring)},
-    setUpLoadFiles = function(filenames = c("upload_percentages.csv", "upload_totalpoints.csv")){
-      private$UpLdFileNames = filenames
-    },
-    getUpLoadFiles = function(){return(private$UpLdFileNames)},
-    exportUpdate = function(uploadFilePath, report = self){ # called from exportReport
-      exportUpdate.REPORT(uploadFilePath, report) 
-    },
-    getTMS = function(){return(private$TMS)},
-    getMissingSections = function(){return(private$MissingSections)}
+    checkTopics = function(){return(private$HasTopics)},
+    enhanceItemInfo = function(report = self, useLocalNames = F, useLocalValues = F){
+      enhanceItemInfo.REPORT(report, useLocalNames, useLocalValues)}, 
+    loadSpecialScoring = function(report = self){ loadSpecialScoring.REPORT(report) }, 
+    updateIRandIRS = function(report = self){updateIRandIRS.REPORT(self)},
+    updateItemInfo = function(x){private$ItemInfo = x}
     
   ) # /public
+  
 ) # /REPORT R6 class
