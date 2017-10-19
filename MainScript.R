085fc96ae31cef273d54bee3b7a1a160bfb33950
debarros <pj.debarros@gmail.com>
2017-10-19 09:39:03 -0400

    Adjusted some stuff in the main script

--- MainScript.R
+++ MainScript.R
@@ -7,6 +7,7 @@ library(openxlsx)
 
 dBtools::UpdateDescription()
 
+
 DataLocation = "C:/week04 (2017-09-26) U1 Numbers Variables Operations"
 ComparisonFileName = "comparison and topic alignment.xlsx"
 ReportFileName = "scores.xlsx"
@@ -14,14 +15,22 @@ TMS = "ScantronAS"
 SMS = "PowerSchool"
 UploadFilenames = c("upload_percentages.csv", "upload_totalpoints.csv")
 
-generateReport(DataLocation = DataLocation)
+generateReport(DataLocation = DataLocation, TMS = TMS)
 
-generateReport(DataLocation = "J:/tests/2017-2018/Math/Alg 1/week04 (2017-09-26) U1 Numbers Variables Operations")
+devtools::install_github()
 
 report = currentReport
-report$getItemInfo()
+result = report$getResults()[[1]]
+report$getResults()
+x = report$getItemInfo()
+
+nchar(x$ItemName)
+
 str(report$getItemInfo())
 str(ItemInfo)
+report$getNarrative()
+
+report$getTopicAlignments()
 
 result1 = report$getResults()[[1]]
 ItemResp = result1$getItemResponses()
@@ -38,3 +47,6 @@ thisSource = "Burgess_p8_itemresponses.csv"
 sourceLocation = "C:/week04 (2017-09-26) U1 Numbers Variables Operations\\exports/Burgess_p8_itemresponses.csv"
 
 itemNames = ItemInfo$ItemName
+
+x = report$getResults()
+str(x, max.level = 1)
