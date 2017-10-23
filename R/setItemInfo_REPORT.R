88e7dc22783b09174609554c35c56fe755142184
debarros <pj.debarros@gmail.com>
2017-10-23 16:42:49 -0400

    setItemInfo method of REPORT class - added code to remove whitespace from the answer key

--- R/setItemInfo_REPORT.R
+++ R/setItemInfo_REPORT.R
@@ -45,6 +45,7 @@ setItemInfo.REPORT = function(report) {
   ItemInfo = ItemInfo[-1,]                                 # remove the first row
   row.names(ItemInfo) = NULL                               # remove the row names
   ItemInfo = as.data.frame(ItemInfo, stringsAsFactors = F) # convert it to a data.frame
+  ItemInfo$`Answer:` = stringr::str_replace_all(ItemInfo$`Answer:`, fixed(" "), "") # remove whitespace from the answer key
  
   # Topic Alignments
   report$setTopicAlignments(ItemInfo) #set the topic alignments
