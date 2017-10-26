7db6b7755bd27d88a590563aa1067b2e4e5e1670
debarros <pj.debarros@gmail.com>
2017-10-26 17:16:51 -0400

    getTopicAlignments method of the REPORT class - eliminated direct access of private member

--- R/getTopicAlignments_REPORT.R
+++ R/getTopicAlignments_REPORT.R
@@ -3,9 +3,9 @@
 getTopicAlignments.REPORT = function(report) {
   
   HasTopics = report$checkTopics()
+  TopicAlignments =  report$getTopicAlignmentsQuick()
   
   if(HasTopics){
-    TopicAlignments =  report$.__enclos_env__$private$TopicAlignments
     for(i in 2:ncol(TopicAlignments)){
       TopicAlignments[,i] = as.integer(TopicAlignments[,i])
     } # /for
