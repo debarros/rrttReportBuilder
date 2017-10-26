5620f9294aad92c08e59c137b7b1e38bfe9e6389
debarros <pj.debarros@gmail.com>
2017-10-26 17:16:08 -0400

    REPORT class - lots of reformatting for readability

--- R/REPORTclass.R
+++ R/REPORTclass.R
@@ -11,12 +11,12 @@ REPORT = R6Class(
   private = list(
     
     # General parameters
-    DistractorCutoffProportion = 0.25, # cutoff percentage for determining whether an item is a distrator
-    EasyCutoff = 0.9,                  # cutoff score for determining whether an item is easy
+    DistractorCutoffProportion = 0.25,                # cutoff percentage for determining whether an item is a distrator
+    EasyCutoff = 0.9,                                 # cutoff score for determining whether an item is easy
     ItemTypeCategories = c("MC","ER","WH","FI","FL"), # all the different item types that are acceptable
-    OverThinkCutoff = 0.1,         # cutoff correlation for determining whether an item is an overthinker
-    RelatedCutoffProportion = 0.2, # target proportion of items to count as highly related
-    PassingScore = 0.7,            # passing score for the test
+    OverThinkCutoff = 0.1,                            # cutoff correlation for determining whether an item is an overthinker
+    RelatedCutoffProportion = 0.2,                    # target proportion of items to count as highly related
+    PassingScore = 0.7,                               # passing score for the test
     #     rules for determining whether an item is Wheat From Chaff
     ChaffRules = data.frame(score = c(.3, .4), correlation = c(.3, .5)), 
     #     lower bound, upper bound, and target proportion for criterion for determining whether an item is difficult
@@ -70,110 +70,110 @@ REPORT = R6Class(
     initialize = function(TMS = "LinkIt"){private$TMS = TMS}, #default the Testing Management System to LinkIt
     
     # Methods to set members
-    setComparison = function(report = self){ setComparison.REPORT(report) }, 
+    setComparison =         function(report = self){ setComparison.REPORT(report) }, 
     setComparisonFileName = function(x){private$ComparisonFileName = x},
     setComparisonLocation = function(x){private$ComparisonLocation = x},
-    setDataLocation = function(x){private$DataLocation = x},
-    setItemInfo = function(report = self){ setItemInfo.REPORT(report) }, 
-    setItemSummary = function(report = self){ setItemSummary.REPORT(report) }, 
-    setHandouts = function(report = self){ setHandouts.REPORT(report) }, 
-    setNarrative = function(report = self){ setNarrative.REPORT(report) },
-    setPassingScore = function(x){private$PassingScore = x},
-    setResults = function(report = self){ setResults.REPORT(report) }, 
-    setSources = function(report = self){ setSources.REPORT(report) }, 
-    setSummary = function(report = self){ setSummary.REPORT(report) }, 
-    setTestName = function(report = self){ setTestName.REPORT(report) }, 
-    setTopicAlignments = function(d2, report = self){ setTopicAlignments.REPORT(d2, report) }, 
-    setUploadTab = function(report = self){ setUploadTab.REPORT(report) },
-    setTopicSummary = function(report = self){ setTopicSummary.REPORT(report) }, 
-    setTopicScores = function(report = self){ setTopicScores.REPORT(report) }, 
-    setUpLoadFiles = function(filenames = c("upload_percentages.csv", "upload_totalpoints.csv")){
-      private$UpLdFileNames = filenames},
+    setDataLocation =       function(x){private$DataLocation = x},
+    setItemInfo =           function(report = self){ setItemInfo.REPORT(report) }, 
+    setItemSummary =        function(report = self){ setItemSummary.REPORT(report) }, 
+    setHandouts =           function(report = self){ setHandouts.REPORT(report) }, 
+    setNarrative =          function(report = self){ setNarrative.REPORT(report) },
+    setPassingScore =       function(x){private$PassingScore = x},
+    setResults =            function(report = self){ setResults.REPORT(report) }, 
+    setSources =            function(report = self){ setSources.REPORT(report) }, 
+    setSummary =            function(report = self){ setSummary.REPORT(report) }, 
+    setTestName =           function(report = self){ setTestName.REPORT(report) }, 
+    setTopicAlignments =    function(d2, report = self){ setTopicAlignments.REPORT(d2, report) }, 
+    setUploadTab =          function(report = self){ setUploadTab.REPORT(report) },
+    setTopicSummary =       function(report = self){ setTopicSummary.REPORT(report) }, 
+    setTopicScores =        function(report = self){ setTopicScores.REPORT(report) }, 
+    setUpLoadFiles =        function(x){private$UpLdFileNames = x},
     
     # Methods to return members
-    getChaffRules = function(){return(private$ChaffRules)},
-    getComparison = function(){return(private$Comparison)},
-    getComparisonLocation = function(){return(private$ComparisonLocation)},
-    getCorrelations = function(){return(private$Correlations)},
-    getDataLocation = function(){return(private$DataLocation)},
-    getDifficultCutoffParams = function(){return(private$DifficultCutoffParams)},
+    getChaffRules =                 function(){return(private$ChaffRules)},
+    getComparison =                 function(){return(private$Comparison)},
+    getComparisonLocation =         function(){return(private$ComparisonLocation)},
+    getCorrelations =               function(){return(private$Correlations)},
+    getDataLocation =               function(){return(private$DataLocation)},
+    getDifficultCutoffParams =      function(){return(private$DifficultCutoffParams)},
     getDistractorCutoffProportion = function(){return(private$DistractorCutoffProportion)},
-    getEasyCutoff = function(){return(private$EasyCutoff)}
-    getHandouts = function(){return(private$Handouts)},
-    getItemInfo = function(){return(private$ItemInfo)},
-    getItemResponseScores = function(){return(private$ItemResponseScores)},
-    getItemScores = function(){return(private$ItemScores)},
-    getItemSummary = function(){return(private$ItemSummary)},
-    getItemTypeCategories = function(){return(private$ItemTypeCategories)},
-    getMissingSections = function(){return(private$MissingSections)},
-    getNarrative = function(){return(private$Narrative)},
-    getOverThinkCutoff = function(){return(private$OverThinkCutoff)},
-    getPassingScore = function(){return(private$PassingScore)},
-    getRelatedCutoffProportion = function(){return(private$RelatedCutoffProportion)},
-    getResponses = function(report = self){ return(getResponses.REPORT(report)) }, 
-    getResponseSet = function(){return(private$ResponseSet)},
-    getResults = function(){return(private$Results)},
-    getSourceFileNames = function(){return(private$SourceFileNames)},
-    getSources = function(){return(private$Sources)},
-    getSpecialScoring = function(){private$SpecialScoring},
-    getSpecialScoringTable = function(){return(private$SpecialScoringTable)},
-    getSummary = function(){return(private$Summary)},
-    getTestName = function(){return(private$TestName)},
-    getTMS = function(){return(private$TMS)},
-    getTopicAlignments = function(report = self){ return(getTopicAlignments.REPORT(report)) }, 
-    getTopicScores = function(){return(private$TopicScores)},
-    getTopicSummary = function(){return(private$TopicSummary)},
-    getUpLoadFiles = function(){return(private$UpLdFileNames)},
-    getUploadTab = function(){return(private$UploadTab)},
-    getUploadTotalPoints = function(){return(private$UploadTotalPoints)},
+    getEasyCutoff =                 function(){return(private$EasyCutoff)}
+    getHandouts =                   function(){return(private$Handouts)},
+    getItemInfo =                   function(){return(private$ItemInfo)},
+    getItemResponseScores =         function(){return(private$ItemResponseScores)},
+    getItemScores =                 function(){return(private$ItemScores)},
+    getItemSummary =                function(){return(private$ItemSummary)},
+    getItemTypeCategories =         function(){return(private$ItemTypeCategories)},
+    getMissingSections =            function(){return(private$MissingSections)},
+    getNarrative =                  function(){return(private$Narrative)},
+    getOverThinkCutoff =            function(){return(private$OverThinkCutoff)},
+    getPassingScore =               function(){return(private$PassingScore)},
+    getRelatedCutoffProportion =    function(){return(private$RelatedCutoffProportion)},
+    getResponses =                  function(report = self){ return(getResponses.REPORT(report)) }, 
+    getResponseSet =                function(){return(private$ResponseSet)},
+    getResults =                    function(){return(private$Results)},
+    getSourceFileNames =            function(){return(private$SourceFileNames)},
+    getSources =                    function(){return(private$Sources)},
+    getSpecialScoring =             function(){return(private$SpecialScoring)},
+    getSpecialScoringTable =        function(){return(private$SpecialScoringTable)},
+    getSummary =                    function(){return(private$Summary)},
+    getTestName =                   function(){return(private$TestName)},
+    getTMS =                        function(){return(private$TMS)},
+    getTopicAlignments =            function(report = self){ return(getTopicAlignments.REPORT(report)) }, 
+    getTopicAlignmentsQuick =       function(){return(private$TopicAlignments)},
+    getTopicScores =                function(){return(private$TopicScores)},
+    getTopicSummary =               function(){return(private$TopicSummary)},
+    getUpLoadFiles =                function(){return(private$UpLdFileNames)},
+    getUploadTab =                  function(){return(private$UploadTab)},
+    getUploadTotalPoints =          function(){return(private$UploadTotalPoints)},
     
     # Methods to quick set members
-    setComparisonQuick = function(x){private$Comparison = x},
-    setCorrelationsQuick = function(x){private$Correlations = x},
-    setDropScoresQuick = function(x){private$DropScores = x},
-    setItemInfoQuick = function(x){private$ItemInfo = x},
+    setComparisonQuick =          function(x){private$Comparison = x},
+    setCorrelationsQuick =        function(x){private$Correlations = x},
+    setDropScoresQuick =          function(x){private$DropScores = x},
+    setItemInfoQuick =            function(x){private$ItemInfo = x},
     setItemResponseOptionsQuick = function(x){private$ItemResponseOptions = x},
-    setItemResponseScoresQuick = function(x){private$ItemResponseScores = x},
-    setItemScoresQuick = function(x){private$ItemScores = x},
-    setHandoutsQuick = function(x){private$Handouts = x},
-    setHasTopicsQuick = function(x){private$HasTopics = x},
-    setMissingSectionsQuick = function(x){private$MissingSections = x},
-    setNarrativeQuick = function(x){private$Narrative = x},
-    setResponseSetQuick = function(x){private$ResponseSet = x},
-    setResultsQuick = function(x){private$Results = x},
-    setSpecialScoring = function(x){private$SpecialScoring = x},
-    setSourceFileNamesQuick = function(x){private$SourceFileNames = x},
-    setSourcesQuick = function(x){private$Sources = x},
-    setSummaryQuick = function(x){private$Summary = x},
-    setTestNameQuick = function(x){private$TestName = x},
-    setTopicAlignmentsQuick = function(x){private$TopicAlignments = x},
-    setTopicScoresQuick = function(x){private$TopicScores = x},
-    setTopicSummaryQuick = function(x){private$TopicSummary = x}, 
-    setUploadTabQuick = function(x){private$UploadTab = x},
-    setUploadTotalPointsQuick = function(x){private$UploadTotalPoints = x},
+    setItemResponseScoresQuick =  function(x){private$ItemResponseScores = x},
+    setItemScoresQuick =          function(x){private$ItemScores = x},
+    setHandoutsQuick =            function(x){private$Handouts = x},
+    setHasTopicsQuick =           function(x){private$HasTopics = x},
+    setMissingSectionsQuick =     function(x){private$MissingSections = x},
+    setNarrativeQuick =           function(x){private$Narrative = x},
+    setResponseSetQuick =         function(x){private$ResponseSet = x},
+    setResultsQuick =             function(x){private$Results = x},
+    setSpecialScoring =           function(x){private$SpecialScoring = x},
+    setSourceFileNamesQuick =     function(x){private$SourceFileNames = x},
+    setSourcesQuick =             function(x){private$Sources = x},
+    setSummaryQuick =             function(x){private$Summary = x},
+    setTestNameQuick =            function(x){private$TestName = x},
+    setTopicAlignmentsQuick =     function(x){private$TopicAlignments = x},
+    setTopicScoresQuick =         function(x){private$TopicScores = x},
+    setTopicSummaryQuick =        function(x){private$TopicSummary = x}, 
+    setUploadTabQuick =           function(x){private$UploadTab = x},
+    setUploadTotalPointsQuick =   function(x){private$UploadTotalPoints = x},
     
     # Export Methods
     exportNarrative = function(report = self){ exportNarrative.REPORT(report) }, 
-    exportReport = function(filename = "scores.xlsx", template = NULL, report = self){ 
+    exportReport =    function(filename = "scores.xlsx", template = NULL, report = self){ 
       exportReport.REPORT(filename, template, report) }, 
-    exportUpdate = function(uploadFilePath, report = self){ # called from exportReport
+    exportUpdate =    function(uploadFilePath, report = self){ # called from exportReport
       exportUpdate.REPORT(uploadFilePath, report) },
-    exportUploads = function(report = self){ exportUploads.REPORT(report) }, 
+    exportUploads =   function(report = self){ exportUploads.REPORT(report) }, 
     
     # Other methods
-    addCorrelations = function(report = self){ addCorrelations.REPORT(report) }, 
-    addItemScores = function(report = self){ addItemScores.REPORT(report) },
+    addCorrelations =        function(report = self){ addCorrelations.REPORT(report) }, 
+    addItemScores =          function(report = self){ addItemScores.REPORT(report) },
     addResponseFrequencies = function(report = self){ addResponseFrequencies.REPORT(report) },
-    applySpecialScoring = function(report = self){ applySpecialScoring.REPORT(report) },
-    badMessage = function(method, report = self){ return(badMessage.REPORT(method, report)) },
-    checkSpecScor = function(){return(private$HasSpecialScoring)},
-    checkStudScor = function(){return(private$HasStudentScoring)},
-    checkTopics = function(){return(private$HasTopics)},
-    enhanceItemInfo = function(report = self, useLocalNames = F, useLocalValues = F){
+    applySpecialScoring =    function(report = self){ applySpecialScoring.REPORT(report) },
+    badMessage =             function(method, report = self){ return(badMessage.REPORT(method, report)) },
+    checkSpecScor =          function(){return(private$HasSpecialScoring)},
+    checkStudScor =          function(){return(private$HasStudentScoring)},
+    checkTopics =            function(){return(private$HasTopics)},
+    enhanceItemInfo =        function(report = self, useLocalNames = F, useLocalValues = F){
       enhanceItemInfo.REPORT(report, useLocalNames, useLocalValues)}, 
-    loadSpecialScoring = function(report = self){ loadSpecialScoring.REPORT(report) }, 
-    updateIRandIRS = function(report = self){updateIRandIRS.REPORT(self)},
-    updateItemInfo = function(x){private$ItemInfo = x}
+    loadSpecialScoring =     function(report = self){ loadSpecialScoring.REPORT(report) }, 
+    updateIRandIRS =         function(report = self){updateIRandIRS.REPORT(self)},
+    updateItemInfo =         function(x){private$ItemInfo = x}
     
   ) # /public
   
