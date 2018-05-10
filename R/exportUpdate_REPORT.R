f72e88eb87aaa0ca175da3c329abed45e04f6d75
debarros <pj.debarros@gmail.com>
2018-05-10 12:29:06 -0400

    exportUpdate method of REPORT class - changed the ScoreUpdates output from text to an html table

--- R/exportUpdate_REPORT.R
+++ R/exportUpdate_REPORT.R
@@ -6,7 +6,7 @@ exportUpdate.REPORT = function(uploadFilePath, report, messageLevel = 0) {
   AllScores = report$getUploadTab()
   testName = report$getTestName()
   DataLocation = report$getDataLocation()
-  scoreupdatefilepath = paste0(DataLocation, "\\", "ScoreUpdates.txt")
+  scoreupdatefilepath = paste0(DataLocation, "\\", "ScoreUpdates.html")
   
   # Get the existing scores and add an identifying code
   ExistingScores = read.csv(uploadFilePath, stringsAsFactors = F)
@@ -20,20 +20,31 @@ exportUpdate.REPORT = function(uploadFilePath, report, messageLevel = 0) {
   
   if(nrow(NewScores) > 0){ # If there are new scores, write a new score update
     
-    # Pad student names to make them the same length
-    maxName = max(nchar(NewScores$StudentName)) 
-    NewScores$StudentName = stringr::str_pad(
-      string = NewScores$StudentName, 
-      width = maxName, 
-      side = "right")
+    # # Pad student names to make them the same length
+    # maxName = max(nchar(NewScores$StudentName)) 
+    # NewScores$StudentName = stringr::str_pad(
+    #   string = NewScores$StudentName, 
+    #   width = maxName, 
+    #   side = "right")
+    
+    # Start the document
+    out = "<!DOCTYPE html>\n<html>\n<body>\n"
     
     # Put in the header line for the score update email
-    out = paste0("Updated scores for ", testName, ":  \n") 
+    out = paste0(out, "<span>Updated scores for </span>", '<span style="font-weight:bold">', testName,'</span>',  ":  \n") 
     
-    # for each score append a line with that student's name, id, and score
+    # Start the table
+    out = paste0(out, '<table style = "border: 1px solid black;">')
+    
+    # for each score append a row with that student's name, id, and score
     for(i in 1:nrow(NewScores)){ 
-      out = paste0(out, "\t",NewScores$StudentName[i], "\t", NewScores$StudentID[i], "\t", NewScores$Percentage[i], "  \n")
+      out = paste0(out,"<tr><td>",NewScores$StudentName[i], "</td>")
+      out = paste0(out,"<td>", NewScores$StudentID[i], "</td>")
+      out = paste0(out,'<td style = "border: 1px solid black; text-align:center;">', NewScores$Percentage[i], "</td></tr> \n")
     } # /for loop
+    
+    # Close the table and the html document
+    out = paste0(out, '</table> \n </body> \n </html>')
     
     write(out, scoreupdatefilepath)  # write the score update emails to a text file
     
