2f9eee2d06a66d65688fc241b5fcd6fc8031a44e
debarros <pj.debarros@gmail.com>
2018-07-01 17:41:45 -0400

    in the getTopicAlignments method of the REPORT class, added a messageLevel statement, added a comment, reformatted some code

--- R/getTopicAlignments_REPORT.R
+++ R/getTopicAlignments_REPORT.R
@@ -2,20 +2,19 @@
 
 getTopicAlignments.REPORT = function(report, messageLevel = 0) {
   
+  if(messageLevel > 1){message("running getTopicAlignments.REPORT")}
+  
   # pull the needed stuff from the report
   HasTopics =        report$checkTopics()
   TopicAlignments =  report$getTopicAlignmentsQuick()
   
+  # Check if there are topics and act accordingly
   if(HasTopics){
-    
     for(i in 2:ncol(TopicAlignments)){
       TopicAlignments[,i] = as.integer(TopicAlignments[,i])
     } # /for
-    
   } else {
-    
     TopicAlignments = NULL
-    
   } # /if-else
   
   return(TopicAlignments)
