c171b19e5a1e459cd4d38eed25e941d8ef310b3e
debarros <pj.debarros@gmail.com>
2019-11-27 11:50:45 -0500

    addResponseFrequencies method of the REPORT class - replaced loop indices with meaningful variable names; reorganized some code so the lines aren't crazy long.

--- R/addResponseFrequencies_REPORT.R
+++ R/addResponseFrequencies_REPORT.R
@@ -17,32 +17,33 @@ addResponseFrequencies.REPORT = function(report, messageLevel = 0) {
   # This currently assumes that, for multiple choice questions, responses are letters in alphabetical order, starting with A
   ItemResponseOptions = vector(mode = "list", length = nrow(ItemInfo))
   names(ItemResponseOptions) = ItemInfo$ItemName
-  for(i in 1:nrow(ItemInfo)){
-    if(ItemInfo$Type[i] == "MC"){
+  for(thisItem in 1:nrow(ItemInfo)){
+    if(ItemInfo$Type[thisItem] == "MC"){
       if(TMS == "ASAP"){                                       # This is a terrible piece of code that should be replaced
-        currentOptions = as.character(1:ItemInfo$options[i])
+        currentOptions = as.character(1:ItemInfo$options[thisItem])
       } else {
-        currentOptions = LETTERS[1:ItemInfo$options[i]]  
+        currentOptions = LETTERS[1:ItemInfo$options[thisItem]]  
       }
-    } else if(ItemInfo$Type[i] == "ER"){
-      currentOptions = as.character(0:ItemInfo$Value[i])
+    } else if(ItemInfo$Type[thisItem] == "ER"){
+      currentOptions = as.character(0:ItemInfo$Value[thisItem])
       if(TMS == "ASAP"){
         if(grepl("global", testName, ignore.case = T) | grepl("US History", testName, ignore.case = T)){
           currentOptions = as.character(seq(from = 0, to = 5, by = .5))
         }
       }
-    } else if(ItemInfo$Type[i] == "WH"){
-      currentOptions = sort(unlist(unique(ItemResponses[,colnames(ItemResponses) == ItemInfo$ItemName[i], with = F])))
-      currentOptions.numeric = suppressWarnings(as.numeric(currentOptions))                                             # Convert to numeric
-      currentOptions.numeric[is.na(currentOptions.numeric)] = currentOptions[is.na(currentOptions.numeric)]             # Anything not numeric, put it back in
-      currentOptions = sort(unique(currentOptions.numeric))                                                             # Remove duplicates from equivalent answers
-    } else if(ItemInfo$Type[i] == "FL"){
-      currentOptions = sort(unlist(unique(ItemResponses[,colnames(ItemResponses) == ItemInfo$ItemName[i], with = F])))
-    } else if(ItemInfo$Type[i] == "FI"){
-      currentOptions = sort(unlist(unique(ItemResponses[,colnames(ItemResponses) == ItemInfo$ItemName[i], with = F])))
+    } else if(ItemInfo$Type[thisItem] == "WH"){
+      currentOptions = sort(unlist(unique(ItemResponses[,colnames(ItemResponses) == ItemInfo$ItemName[thisItem], with = F])))
+      currentOptions.numeric = suppressWarnings(as.numeric(currentOptions))     # Convert to numeric
+      naNumOpts = is.na(currentOptions.numeric)
+      currentOptions.numeric[naNumOpts] = currentOptions[naNumOpts]             # Anything not numeric, put it back in
+      currentOptions = sort(unique(currentOptions.numeric))                     # Remove duplicates from equivalent answers
+    } else if(ItemInfo$Type[thisItem] == "FL"){
+      currentOptions = sort(unlist(unique(ItemResponses[,colnames(ItemResponses) == ItemInfo$ItemName[thisItem], with = F])))
+    } else if(ItemInfo$Type[thisItem] == "FI"){
+      currentOptions = sort(unlist(unique(ItemResponses[,colnames(ItemResponses) == ItemInfo$ItemName[thisItem], with = F])))
     } # /if-else
     currentOptions = currentOptions[currentOptions != "--"]
-    ItemResponseOptions[[i]] = currentOptions
+    ItemResponseOptions[[thisItem]] = currentOptions
   } # /for
   
   
@@ -51,8 +52,8 @@ addResponseFrequencies.REPORT = function(report, messageLevel = 0) {
   names(responseSetByType) = unique(ItemInfo$Type)
   
   # For each type, determine the possible options
-  for(i in 1:length(responseSetByType)){
-    allresponseoptions = unlist(ItemResponseOptions[ItemInfo$Type == names(responseSetByType)[i]])
+  for(thisRespType in 1:length(responseSetByType)){
+    allresponseoptions = unlist(ItemResponseOptions[ItemInfo$Type == names(responseSetByType)[thisRespType]])
     uniqueResponseOptions = unique(allresponseoptions)
     uniqueResponseOptions.numeric = suppressWarnings(as.numeric(uniqueResponseOptions))
     if(any(is.na(uniqueResponseOptions.numeric))){                                       # if any options are not numbers,
@@ -60,20 +61,20 @@ addResponseFrequencies.REPORT = function(report, messageLevel = 0) {
     } else {                                                                             # if all options are numbers,
       responsesToUse = as.character(sort(uniqueResponseOptions.numeric))                 # sort numerically
     }
-    responseSetByType[[i]] = responsesToUse
+    responseSetByType[[thisRespType]] = responsesToUse
   } # /for
   
   # Initialize the responseSet vector 
   responseSet = rep("", times = max(lengths(responseSetByType)))
   
   
-  for(i in 1:length(responseSetByType)){                                   # For each element in responseSetByType,
-    if(length(responseSetByType[[i]]) > 0){                                # If there are any responses for that type,
-      for(j in 1:length(responseSetByType[[i]])){                          # For each entry in that vector, 
-        if(nchar(responseSet[j]) > 0){                                     # if there is aleady something there
-          responseSet[j] = paste0(responseSet[j], "/")                     # append a slash
+  for(thisRespType in 1:length(responseSetByType)){                                   # For each element in responseSetByType,
+    if(length(responseSetByType[[thisRespType]]) > 0){                                # If there are any responses for that type,
+      for(thisRespOption in 1:length(responseSetByType[[thisRespType]])){                          # For each entry in that vector, 
+        if(nchar(responseSet[thisRespOption]) > 0){                                     # if there is aleady something there
+          responseSet[thisRespOption] = paste0(responseSet[thisRespOption], "/")                     # append a slash
         }
-        responseSet[j] = paste0(responseSet[j], responseSetByType[[i]][j]) # append the response option
+        responseSet[thisRespOption] = paste0(responseSet[thisRespOption], responseSetByType[[thisRespType]][thisRespOption]) # append the response option
       } # /for each response option for this type
     }
   } # /for each type of response
@@ -82,16 +83,16 @@ addResponseFrequencies.REPORT = function(report, messageLevel = 0) {
   # Add frequency columns to ItemInfo and load the response frequencies
   basecolumn = ncol(ItemInfo)                       # How many columns does ItemInfo have already?
   ItemInfo[,responseSet] = ""                       # Initialize columns for response frequencies
-  for(i in 1:nrow(ItemInfo)){                       # For each item
-    if(ItemInfo$Type[i] %in% c("ER", "MC")){
-      curRespSet = ItemResponseOptions[[i]]
+  for(thisItem in 1:nrow(ItemInfo)){                       # For each item
+    if(ItemInfo$Type[thisItem] %in% c("ER", "MC")){
+      curRespSet = ItemResponseOptions[[thisItem]]
     } else {
-      curRespSet = responseSetByType[[ItemInfo$Type[i]]]  
+      curRespSet = responseSetByType[[ItemInfo$Type[thisItem]]]  
     }
     
     for(j in 1:length(curRespSet)){   # For each possible option for that item type category,
       currentResponse = curRespSet[j] # Grab the response
-      ItemInfo[i,j+basecolumn] = sum(ItemResponses[,ItemInfo$ItemName[i], with = F] == currentResponse, na.rm = T) # Get the frequency
+      ItemInfo[thisItem,j+basecolumn] = sum(ItemResponses[,ItemInfo$ItemName[thisItem], with = F] == currentResponse, na.rm = T) # Get the frequency
     }
   }
   
